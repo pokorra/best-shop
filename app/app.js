fa9b4df8384ceb65f792d65d499fcc0ae76cf706
pokorra <patrycja.pokora@gmail.com>
2020-10-13 20:44:13 +0200

    added scrolling

--- app/app.js
+++ app/app.js
@@ -10,3 +10,32 @@ toggleButton.addEventListener('click', ()=>{
         item.classList.toggle('span-cross')
     } )
 })
+
+// scrolling
+const main = document.querySelector('#main');
+const why = document.querySelector('#why');
+const benefits = document.querySelector('#benefits');
+const prices = document.querySelector('#prices');
+const contact = document.querySelector('#contact');
+const best = document.querySelector('.logo_best_shop');
+
+const toWhy =  document.querySelector('.to-why');
+const toBens =  document.querySelector('.to-bens');
+const toPrices = document.querySelector('.to-prices');
+const toCont =  document.querySelector('.to-cont');
+const toTop =  document.querySelector('.to-top');
+
+
+function Smooth(from, to){
+    to.addEventListener('click', ()=>{
+        from.scrollIntoView({behavior: "smooth"});
+        console.log(from, to);
+    })
+};
+
+Smooth(why, toWhy);
+Smooth(benefits, toBens);
+Smooth(prices, toPrices);
+Smooth(contact, toCont);
+Smooth(main, toTop);
+Smooth(main, best);
