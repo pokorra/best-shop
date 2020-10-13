55cc8086af8436e1369e384c5b989bed14716513
pokorra <patrycja.pokora@gmail.com>
2020-10-13 13:09:15 +0200

    animated hamburger, proceeding with media queries

--- app/app.js
+++ app/app.js
@@ -1,8 +1,12 @@
 const toggleButton = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.menu_nav');
 const menuContainer = document.querySelector('.menu')
+const hamSpans = document.querySelectorAll('.ham-span');
 
 toggleButton.addEventListener('click', ()=>{
     navLinks.classList.toggle('active')
     menuContainer.classList.toggle('wider')
+    hamSpans.forEach((item) => {
+        item.classList.toggle('span-cross')
+    } )
 })
